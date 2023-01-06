1d465f98669fcd595bfea62d38309fe3242f563d
pm <piotr.mroz@neosystem.pl>
2023-01-06 20:34:18 +0100

    added docker and docker-compose support

--- HousingMarketAdvisor.API/Program.cs
+++ HousingMarketAdvisor.API/Program.cs
@@ -35,7 +35,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection();
 
 
 app.UseCors("AllowAllOrigins");
--- HousingMarketAdvisor.Gateway/Program.cs
+++ HousingMarketAdvisor.Gateway/Program.cs
@@ -1,4 +1,5 @@
 using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -21,10 +22,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection();
 
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
